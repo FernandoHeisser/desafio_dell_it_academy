32e5218a75e2e51737a57a648d922cb88b06b50a
FernandoHeisser <fernando.heisserch@gmail.com>
2020-09-03 01:59:23 -0300

    Adicionado readline

--- server/src/Controllers/FileController.ts
+++ server/src/Controllers/FileController.ts
@@ -1,8 +1,26 @@
 import {Request, Response} from 'express'
+import readline from 'readline';
+import fs from 'fs';
 
 class FileController {
     execute(request: Request, response: Response) {
+        let array: string[] = [];
         
+        const rl = readline.createInterface({
+            input: fs.createReadStream('src/uploads/file.txt'),
+            output: process.stdout,
+            terminal: false
+        });
+
+        rl.on('line', function(line) {
+            array.push(line);
+        });
+
+        rl.on('close', function() {
+            let responseToSend = {input: array, output: []};
+
+            return response.status(200).json(responseToSend);
+        });
     }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,23 @@
 import express from 'express'
+import multer from 'multer'
+
 import FileController from './Controllers/FileController'
 
 const routes = express.Router();
 const fileController = new FileController;
 
-routes.post('/execute', fileController.execute);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        cb(null, 'src/uploads/');
+    },
+    filename: function (req, file, cb) {
+
+        cb(null, 'file.txt')
+    }
+});
+const upload = multer({ storage });
+
+routes.post('/execute', upload.single('file'), fileController.execute);
 
 export default routes;
